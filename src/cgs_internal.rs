2aad6df51e4b42c635fcea763701e74da673632d
Zhuoran Tan <t@zhuorantan.com>
2022-03-15 18:15:57 +0800

    Implement unlock

--- src/cgs_internal.rs
+++
@@ -1,9 +0,0 @@
-use std::os::raw::c_int;
-use core_foundation::array::CFArrayRef;
-use core_foundation::string::CFStringRef;
-use core_foundation::dictionary::CFDictionaryRef;
-
-extern "C" {
-    pub fn CGSCopyCurrentSessionDictionary() -> CFDictionaryRef;
-    pub fn CGSSessionCreateSessionIDWithOptions(path: CFStringRef, argv: CFArrayRef, flags: c_int, outSession: *mut c_int);
-}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 use std::mem;
 use std::ptr;
 use std::os::raw::c_int;
+use std::ffi::c_void;
 use core_foundation::base::*;
 use core_foundation::array::*;
 use core_foundation::string::*;
 use core_foundation::number::*;
 use core_foundation::dictionary::*;
 
-mod cgs_internal;
+type CFObject = *mut c_void;
+type CFNotificationCenterRef = CFObject;
+
+const K_CFNOTIFICATION_POST_TO_ALL_SESSIONS: u64 = 1 << 1;
+
+extern "C" {
+    fn CGSCopyCurrentSessionDictionary() -> CFDictionaryRef;
+    fn CGSSessionCreateSessionIDWithOptions(path: CFStringRef, argv: CFArrayRef, flags: c_int, outSession: *mut c_int);
+
+    fn CFNotificationCenterGetDistributedCenter() -> CFNotificationCenterRef;
+    fn CFNotificationCenterPostNotificationWithOptions(center: CFNotificationCenterRef, name: CFStringRef, object: CFTypeRef, userInfo: CFDictionaryRef, flags: u64);
+}
 
 fn get_session_id() -> c_int {
-    let dict_ref = unsafe { cgs_internal::CGSCopyCurrentSessionDictionary() };
+    let dict_ref = unsafe { CGSCopyCurrentSessionDictionary() };
 
     let key = CFString::new("kCGSSessionIDKey");
-    let mut number_ref: CFTypeRef = std::ptr::null();
+    let mut number_ref: CFTypeRef = ptr::null();
 
     unsafe { CFDictionaryGetValueIfPresent(dict_ref, key.to_void(), &mut number_ref) };
     unsafe { CFRelease(dict_ref.to_void()) };
@@ -36,7 +48,7 @@ fn lock_screen(msg: &str) {
     ];
 
     let lock_cmd_args: Vec<CFString> = lock_cmd_args.iter().map(|x| CFString::new(x)).collect();
-    let lock_cmd_args: Vec<CFStringRef> = lock_cmd_args.iter().map(|x| unsafe { std::mem::transmute(x.to_void()) }).collect();
+    let lock_cmd_args: Vec<CFStringRef> = lock_cmd_args.iter().map(|x| unsafe { mem::transmute(x.to_void()) }).collect();
 
     let lock_cmd_args_array = unsafe { 
         CFArrayCreate(kCFAllocatorDefault,
@@ -46,10 +58,24 @@ fn lock_screen(msg: &str) {
     };
 
     let mut out_session: c_int = 0;
-    unsafe { cgs_internal::CGSSessionCreateSessionIDWithOptions(lock_cmd_args[0], lock_cmd_args_array, 3, &mut out_session) };
+    unsafe { CGSSessionCreateSessionIDWithOptions(lock_cmd_args[0], lock_cmd_args_array, 3, &mut out_session) };
     unsafe { CFRelease(lock_cmd_args_array.to_void()) };
 }
 
+fn unlock_screen() {
+    let center = unsafe { CFNotificationCenterGetDistributedCenter() };
+    let name = CFString::new("com.apple.remotedesktop.stopLockScreen");
+
+    unsafe {
+        CFNotificationCenterPostNotificationWithOptions(center,
+                                                        mem::transmute(name.to_void()),
+                                                        ptr::null(),
+                                                        ptr::null(),
+                                                        K_CFNOTIFICATION_POST_TO_ALL_SESSIONS)
+    };
+}
+
 fn main() {
-    lock_screen("foobar");
+    // lock_screen("foobar");
+    unlock_screen();
 }
